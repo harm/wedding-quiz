baf20ad30694ffabd7b89f5d5d11a0487f08892e
Harm de Laat <harm@kabisa.nl>
2012-08-21 21:21:15 +0200

    added round 1

--- js/smart.js
+++ js/smart.js
@@ -1,20 +1,4 @@
-var round1_questions = [
-	"Waar kikker je van op?",
-	"Wat bevindt zich niet in de kelder van Dries?",
-	"Wat kan Lian goed masseren?",
-	"Als Dries gaat wakeboarden, gaat hij dan rechts of linksom?",
-	"Bij wie gaan wel eens de lampjes uit?",
-	"Hoe eindigt een voetbalfeestje voor Lian?",
-	"Hoeveel kilogram scampis heeft Dries in Spa gegeten?",
-	"Wat is volgens Dries altijd helemaal de max?",
-	"Waar zat Dries altijd toen hij nog bij Rutten werkte?",
-	"Hoeveel graden was het in de huiskamer tijdens het weekend in Spa?",
-	"Hoe ziet Lian haar droom vakantieverblijf er uit?",
-	"Hoe heet de minnaar van Lian?",
-	"Wie drikt als Dries en Lian met de auto op stap zijn?",
-	"Waar heeft Dries met moeite afstand van genomen sinds zijn relatie met Lian?",
-	"In welk jaartal is de badkamer van Dries en Lian afgerond?"
-];
+
 
 
 
